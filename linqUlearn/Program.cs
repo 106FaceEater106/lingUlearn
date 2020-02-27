99eed30fb4212b4aca4e4ba3c9979893e56c4934
Madiyar <nu_ofsn11@mail.ru>
2020-02-27 15:24:24 +0600

    Add project files.

---
+++ linqUlearn/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace linqUlearn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Linq");
+            Console.ReadKey();
+        }
+    }
+}
