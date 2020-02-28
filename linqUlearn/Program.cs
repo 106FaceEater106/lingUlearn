e316bcf950e4f219346f5cac73f519e8e12f31f0
Madiyar <nu_ofsn11@mail.ru>
2020-02-28 11:20:03 +0600

    method chaining 2

--- linqUlearn/Program.cs
+++ linqUlearn/Program.cs
@@ -10,14 +10,16 @@ namespace linqUlearn
         static void Main(string[] args)
         {
             int[] nums = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-            IEnumerable<int> ien = nums.Where(x => x % 2 == 0)
-                .Select(x => x * x)
-                .Skip(1)
-                .Take(2)
-                .ToArray();
-            
-            ShowRes(ien);
-            Console.WriteLine(ien.Count());
+            var people =  new[]{ "Pavel Egorov", "Yuriy Okulovskiy",
+    "Alexandr Denisov", "Ivan Sorokin",
+    "Dasha Zubova", "Irina Gess"};
+
+            var names = people.Select(fn => fn.Split(' ')[0]);
+
+            var girls = names.Where(name => name[name.Length - 1] == 'a');
+
+            ShowRes(names);
+            ShowRes(girls);
             Console.WriteLine("Linq");
             Console.ReadKey();
         }
@@ -29,6 +31,14 @@ namespace linqUlearn
                 Console.WriteLine(el);
             }
         }
-       
+        public static void ShowRes(IEnumerable<string> mas)
+        {
+            foreach (var el in mas)
+            {
+                Console.WriteLine(el);
+            }
+        }
+
+
     }
 }
