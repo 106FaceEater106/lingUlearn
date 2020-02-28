0ce47508faee2b1063d94c7b8a00a5d564a561a8
Madiyar <nu_ofsn11@mail.ru>
2020-02-28 09:56:50 +0600

    first example

--- linqUlearn/Program.cs
+++ linqUlearn/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace linqUlearn
 {
@@ -6,9 +8,21 @@ namespace linqUlearn
     {
         static void Main(string[] args)
         {
-            
+            int[] nums = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            //List<int> even = nums.Where(x => x % 2 == 0);
+            IEnumerable<int> even = nums.Where(x => x % 2 == 0);
+            ShowRes(even);
             Console.WriteLine("Linq");
             Console.ReadKey();
         }
+
+        public static void ShowRes(IEnumerable<int> mas)
+        {
+            foreach(var el in mas)
+            {
+                Console.WriteLine(el);
+            }
+        }
+       
     }
 }
